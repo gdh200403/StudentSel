640490218f2084f710e6e35a126fd589a9edaf05
Sinady <yza03@mail.ustc.edu.cn>
2023-06-04 11:37:44 +0800

    report

--- src/test/java/com/cy/studentsel/StudentSelApplicationTests.java
+++ src/test/java/com/cy/studentsel/StudentSelApplicationTests.java
@@ -14,21 +14,9 @@ import java.sql.SQLException;
 class StudentSelApplicationTests {
     @Autowired
     private DataSource DataS;
-    @Qualifier("StudentDAO")
-    @Autowired
-    private StudentDAO student;
-
-    @Test
-    void contextLoads() {
-    }
 
     @Test
     void getConnection() throws SQLException {
         System.out.println(DataS.getConnection());
-//        System.out.println(student.getStudent("2019210001"));
-//        System.out.println(student.login("2019210001","123456"));
-//        System.out.println(student.addStudent(new StudentRecord("PB92000001","张百嘉","男",20,"计算机科学与技术","123456")));
-//        System.out.println(student.queryAllStudent());
     }
-
 }
